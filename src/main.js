6b01d2c5b3b4b32cdea93fe6d862b77fd21862ee
woke <1076458624@qq.com>
2019-11-06 17:33:19 +0800

    修改代码

--- src/main.js
+++ src/main.js
@@ -1,17 +1,28 @@
 import Vue from "vue";
 import App from "./page/layout";
-import { Pagination,Tag,Timeline,Card,TimelineItem,Loading } from "element-ui";
+import {
+  Pagination,
+  Tag,
+  Timeline,
+  Card,
+  TimelineItem,
+  Loading
+} from "element-ui";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import Router from "vue-router";
 import store from "./vuex/store";
-import "@/theme/index.css";
 import routerConfig from "./router.config";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueAxios, axios);
-Vue.use(Pagination).use(Tag).use(Timeline).use(Card).use(TimelineItem).use(Loading);
+Vue.use(Pagination)
+  .use(Tag)
+  .use(Timeline)
+  .use(Card)
+  .use(TimelineItem)
+  .use(Loading);
 
 Vue.use(Router);
 const router = new Router({
