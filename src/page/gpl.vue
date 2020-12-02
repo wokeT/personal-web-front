3cf05657778c6031a4d2e1928ea3965153daecf8
woke <1076458624@qq.com>
2020-12-02 15:18:01 +0800

    update:update api url

--- src/page/gpl.vue
+++ src/page/gpl.vue
@@ -1,39 +1,46 @@
+<!--
+ * @Author: woke
+ * @Date: 2019-12-03 14:41:11
+ * @LastEditors: 
+ * @LastEditTime: 2020-12-02 15:16:26
+ * @Description: 
+-->
 <template>
   <div>
-    <GplCard :list="list" :loading='loading' />
+    <GplCard :list="list" :loading="loading" />
   </div>
 </template>
 
 <script>
-import GplCard from "@/components/gpl/gplCard";
+import GplCard from '@/components/gpl/gplCard'
 export default {
-  data: function() {
+  data: function () {
     return {
       list: [],
       loading: false,
-    };
+    }
   },
   components: {
     GplCard
   },
   methods: {
-    async getPage() {
-      this.loading = true;
+    async getPage () {
+      this.loading = true
       try {
-        let res = await this.axios.get('/gpl/users/wokeT/repos');
-        this.list = res.data;
+        let res = await this.axios.get('/gpl/users/woke-T/repos')
+        this.list = res.data
       } catch (e) {
         this.$notify.error({
-          title: "出错了",
+          title: '出错了',
           message: e.message
-        });
+        })
       } finally {
-        this.loading = false;
+        this.loading = false
       }
     }
   },
-  async mounted() {
-    await this.getPage();
+  async mounted () {
+    await this.getPage()
   }
 };
 </script>
